a6ab39e18fb8005ce82ab5134f50eab55bab2907
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-07-12 14:05:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, occassion){
+    const messages = []
+    for(let i = 0; i<names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occassion} gift!`)
+    }
+    return messages
+}
+
+const messages = writeCards(["Charlie", "Samip", "Ali"], "surprise");
+console.log(messages);
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+ }
+
+const countdown = countDown(10);
+console.log(countdown);
